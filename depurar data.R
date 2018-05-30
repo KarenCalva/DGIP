ab7e7cb041c24e13bdbcc06331cd03fb2976a934
KarenCalva <karenpris5792@hotmail.com>
2018-05-30 08:19:48 -0500

    depurar la data 1

--- depurar data.R
+++ depurar data.R
@@ -1,16 +1,52 @@
-
+library(data.table)
+library(dplyr)
 load("dt.RData")
 
-# periodo
+key(dt) # "codigoEstudiante" "periodo"
+
+## codigoEstudiante
+
+# hay codest de 9 7 6 y 5 caracteres
+unique(nchar(dt$codigoEstudiante)) 
+
+## periodo
 
 # # periodo como una vairbale tipo factor
 dt$periodo <- as.factor(dt$periodo)
 
 length(levels(dt$periodo)) # hay 20 periodos, desde el 2009-1 hasta el 2018-A
 
+levels(dt$periodo) # periodos
+
+sum(is.na(dt$periodo)) # está completo
+grep("NULL",dt$periodo) # 0 NULL's
+
+## numMatricula
+
+sum(is.na(dt$numMatricula)) # está completo
+grep("NULL",dt$numMatricula) # 0 NULL's
+
+dt$numMatricula <- as.factor(dt$numMatricula) # variable tipo entera
+
+levels(dt$numMatricula) # "1" "2" "3"
+
+## paralelo
+
+sum(is.na(dt$paralelo)) # está completo
+grep("NULL",dt$paralelo) # 0 NULL's
+
+dt$paralelo <- as.factor(dt$paralelo)
+levels(dt$paralelo) # alfanumericos y alfabeticos
+
+# por periodo hay un tipo de paralelos
+
+dt <- dt %>% group_by(codigoEstudiante,periodo,numMatricula)
 
 
-d0$sexoes <- as.factor(d0$sexoes)
+
+
+
+
 d0
 
 d1$periodo <- as.factor(d1$periodo)
